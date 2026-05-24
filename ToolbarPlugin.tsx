5263bb3a65c2f2ddd943a6854c4b4d6c24b4a6ed
Ankit Bourasu <ankitbourasi0@gmail.com>
2026-05-25 03:11:55 +0530

    Visually Headings Apply

--- ToolbarPlugin.tsx
+++ ToolbarPlugin.tsx
@@ -6,11 +6,27 @@ we use useLexicalComposerContext to get access to the editor.
 we then send a FORMAT_TEXT_COMMAND to tell Lexical to update its internal state.
 */
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
-import { FORMAT_TEXT_COMMAND } from 'lexical'
+import { FORMAT_TEXT_COMMAND, $getSelection, $isRangeSelection } from 'lexical'
+
+import {$setBlocksType} from '@lexical/selection'
+import {$createHeadingNode} from '@lexical/rich-text'
+
+
 const ToolbarPlugin = () => {
 
   //This HOOK give us access to the underlying Lexical Editor Instance, 
   const [editor] = useLexicalComposerContext()
+
+
+  //Convert to convert  the current line into a HEADING.
+  const formatHeading = (headingSize : 'h1' | 'h2' | 'h3') =>{
+    editor.update(() => {
+      const selection = $getSelection()
+      if($isRangeSelection(selection)){
+        $setBlocksType(selection, ()=> $createHeadingNode(headingSize));
+      }
+    })
+  }
   return (
     <div className="flex gap-2 mb-6 pb-4 border-b border-gray-200">
       <button
@@ -33,6 +49,18 @@ const ToolbarPlugin = () => {
         U
       </button>
 
+      <button onClick={() => formatHeading('h1')}
+        className="px-3 py-1 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-100 font-bold text-gray-900"
+      >
+        H1
+      </button>
+
+
+      <button onClick={() => formatHeading('h2')}
+        className="px-3 py-1 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-100 font-bold text-gray-800"
+      >
+        H2
+      </button>
     </div>
   )
 }
